83d4fb4ee7bc7482ce1578c29e6902b0dc03c2ad
Jin mo Kim <skim112790@yahoo.com>
2022-02-15 21:28:21 -0500

    Added 404 page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // require needed modules
 const express = require("express")
+require("dotenv").config()
 
 // initialize app
 const app = express()
@@ -13,7 +14,16 @@ app.get('/', (req, res) => {
     `)
 })
 
+// Wildcard Route
+app.get('*', (req, res) => {
+    res.status(404).send(`
+    <h1>
+        This Page does not exist. Go away :)
+    </h1>
+    `)
+})
+
 // Listen
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Listening to port 3000")
 })
