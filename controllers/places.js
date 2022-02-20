c1b6d057896566c6658a728fd2a1997c26e8f352
Jin mo Kim <skim112790@yahoo.com>
2022-02-20 12:43:51 -0500

    Added places view with placeholder data

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,20 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    let places = [{
+        name: 'Chok-Dee',
+        city: 'Atlanta',
+        state: 'WA',
+        cuisines: 'Thai',
+        pic: 'http://placekitten.com/250/250'
+    }, {
+        name: 'Mochinut',
+        city: 'Wanganui',
+        state: 'NZ',
+        cuisines: 'Mochi-Donuts, Hot Dogs',
+        pic: 'https://place-puppy.com/250x250'
+    }]
+    res.render('places/index', {places})
 })
 
 module.exports = router
