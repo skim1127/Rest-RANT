9a4211ad663e82949874404df0383d1e6d47da85
Jin mo Kim <skim112790@yahoo.com>
2022-02-22 21:49:43 -0500

    continue from REST-Rant part 6, Part Four: Implement Method Override

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,44 @@
 const React = require('react')
 const Def = require('../default')
 
-function show() {
+function show(data) {
+    console.log(data)
     return (
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <h1>{ data.place.name }</h1>
+                <div>
+                    <img src={`${ data.place.pic }`}/>
+                </div>
+                <div>
+                    <h2>
+                        Description
+                    </h2>
+                    <h3>
+                        { data.place.city }, 
+                        { data.place.state },  
+                        serving { data.place.cuisines }
+                    </h3>
+                </div>
+                <section>
+                    <h2>
+                        Rating
+                    </h2>
+                    <h3>
+                        Currently Unrated
+                    </h3>
+                </section>
+                <hr/>
+                <div>
+                    <h2>
+                        Comments
+                    </h2>
+                    <h3>
+                        No comments yet!
+                    </h3>
+                </div>
             </main>
+
         </Def>
     )
 }
