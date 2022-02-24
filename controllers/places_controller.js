efe3dd83c8be88db74fa5815743270434a763c92
Jin mo Kim <skim112790@yahoo.com>
2022-02-24 18:12:02 -0500

    Delete function added

--- controllers/places_controller.js
+++ controllers/places_controller.js
@@ -47,4 +47,19 @@ router.get('/', (req, res) => {
     res.render('places/index', {places})
 })
 
+// DELETE
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
+})
+
 module.exports = router
